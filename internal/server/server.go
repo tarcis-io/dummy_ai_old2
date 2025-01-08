983cbd116fbe58a3b5a413edd9c1b717e1532761
Tarcisio Oliveira <t@rcis.io>
2025-01-08 15:20:06 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,3 +15,11 @@ func Start() {
 		panic(err)
 	}
 }
+
+func serveFile(route string, file string) {
+
+	http.HandleFunc(route, func(responseWriter http.ResponseWriter, request *http.Request) {
+
+		http.ServeFile(responseWriter, request, file)
+	})
+}
