a773bc9b7e20c2a5974960eed82a8e47fec3e970
Tarcisio Oliveira <t@rcis.io>
2025-01-07 22:16:24 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,17 @@
 package server
 
+import (
+	"net/http"
+)
+
+import (
+	"dummy_ai/internal/env"
+)
+
 func Start() {
+
+	if err := http.ListenAndServe(env.ServerAddress(), nil); err != nil {
+
+		panic(err)
+	}
 }
