116417b083b780b28f6b19f316f2d395949ab979
Tarcisio Oliveira <t@rcis.io>
2025-01-09 14:09:16 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,9 +11,17 @@ import (
 func Start() {
 
 	staticFiles := map[string]string{
-		"/manifest.json": "./static/manifest.json",
-		"/robots.txt":    "./static/robots.txt",
-		"/sitemap.xml":   "./static/sitemap.xml",
+		"/manifest.json":            "./static/manifest.json",
+		"/robots.txt":               "./static/robots.txt",
+		"/sitemap.xml":              "./static/sitemap.xml",
+		"/apple_touch_icon.png":     "./static/img/favicon/apple_touch_icon.png",
+		"/favicon.ico":              "./static/img/favicon/favicon.ico",
+		"/favicon.svg":              "./static/img/favicon/favicon.svg",
+		"/favicon_192.png":          "./static/img/favicon/favicon_192.png",
+		"/favicon_512.png":          "./static/img/favicon/favicon_512.png",
+		"/favicon_512_maskable.png": "./static/img/favicon/favicon_512_maskable.png",
+		"/logo.svg":                 "./static/img/logo/logo.svg",
+		"/logo_white.svg":           "./static/img/logo/logo_white.svg",
 	}
 
 	for route, file := range staticFiles {
